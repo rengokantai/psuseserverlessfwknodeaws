a41805626f1928d97d20f1c1ee6b07a4e029a421
Hernan Y.Ke <yuriqiao@gmail.com>
2017-12-16 15:59:33 -0500

    add libs

--- handler.js
+++ handler.js
@@ -1,16 +1,76 @@
 'use strict';
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+const twilioAccountSid = process.env.TWILIOID
+const twilioAccountKey = process.env.TWILIOKEY
+const twilioPhoneNumber = process.env.TWILIONUMBER
+const recaptchaSecret = process.env.RECAPTCHAKEY
+
+const request = require('request')
+const twilioClient = require('twilio')(twilioAccountSid,twilioAccountKey)
+
+const headers = {'Access-Control-Allow-Origin':"*"}
+
+module.exports.validateAndSend = (event,context,callback)=>{
+  const validationData = {
+    url:'https://www.google.com/recaptcha/api/siteverify?secret='+recaptchaSecret+"&response="+event.body.captcha,
+    method:'POST',
   };
 
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
-};
+  request(validationData,function(error,response,body){
+    const parsedBody = JSON.parse(body)
+    if(error||response.statusCode!==200){
+      const recaptchaErrResponse = {
+        headers:headers,
+        statusCode:500,
+        body:JSON.stringify({
+          status:'fail',
+          message:'Error attempting to validate recaptcha',
+          error:error|| response.statusCode
+        })
+      }
+      return callback(null,recaptchaErrResponse)
+    } else if (parsedBody.success === false){
+      const recaptchaFailedErrResponse = {
+        headers:headers,
+        statusCode:200,
+        body:JSON.stringify({
+          status:'fail',
+          message:'Captcha validation failed',
+        })
+      }
+      return callback(null,recaptchaFailedErrResponse)
+    } else if(parsedBody.success === true){
+      const sms = {
+        to: event.body.to,
+        body:event.body.message ||'',
+        from: twilioPhoneNumber,
+      }
+      twilioClient.messages.create(sms,(error,data)=>{
+        if(error){
+          const twilioErrResponse = {
+            headers:headers,
+            statusCode:200,
+            body:JSON.stringify({
+              status:'fail',
+              message:error.message,
+              error:error
+            })
+          }
+          return callback(null,twilioErrResponse)
+        }
+        const successResponse = {
+          headers:headers,
+          statusCode:200,
+          body:JSON.stringify({
+            status:'success',
+            message:'successful sent',
+            body:data.body,
+            created:data.dateCreated
+          })
+        }
+        callback(null,twilioErrResponse)
+        
+      })
+    }
+  })
+}
